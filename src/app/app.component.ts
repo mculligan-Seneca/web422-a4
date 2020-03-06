cd73c59a61c5206fec12c486d4fd440f04447011
Mitchell Culligan <mitchellculligan@Mitchells-MacBook-Pro.local>
2020-03-06 12:31:59 -0500

    nice;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,10 @@
+/************************************************************************ *********
+* WEB422 – Assignment 04
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students. *
+* Name: ________Mitchell Culligan______________ Student ID: ____161293170__________ Date: _____March 6th, 2020___________
+* ********************************************************************************/
 import { Component } from '@angular/core';
 
 @Component({
